9d76754567acc06e7a50955ab5ceca801ad5de08
Simon Balean <simonbalean@gmail.com>
2018-11-27 10:49:39 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -79,7 +79,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[:yukihiro_matsumoto][:known_for]
+programmer_hash[:yukihiro_matsumoto] = {known_for: Ruby},{
     
 end
 
